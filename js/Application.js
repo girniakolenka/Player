9b8378efcf4d7e940387fb0267baf4e6d92c7825
tmptrash <tmptrash@mail.ru>
2013-11-25 12:17:43 +0200

    + Added comments...

--- js/Application.js
+++ js/Application.js
@@ -2,14 +2,18 @@ N13.define('App.Application', {
     extend   : 'App.base.App',
     requires : [
         'App.controller.player.Player',
-        // We don't need a Config here!
+        // db: We don't need a Config here!
         'App.Config'
     ],
+
+    // db: Why do you need an empty method here?
     init     : function() {
         this.callParent(arguments);
     },
 
     run      : function() {
+        // db: it's better to describe here the autoRun config set to true for this controller
+        // db: it's hard to understand that from the code
         N13.create('App.controller.player.Player');
     }
 })
